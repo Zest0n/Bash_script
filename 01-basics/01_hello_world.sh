159b2e83821b4533460a85b6fc8a49f93305a680
Zest0n <abhaystark5780@gmail.com>
2026-08-16 20:19:39 +0530

    added my first hello world script

---
+++ 01-basics/01_hello_world.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#My first Bash script
+echo "Hello, World~"
+#it is really crazy how everything starts right here
+#I think I had a moment of peace while writing this, and I felt happy :>
