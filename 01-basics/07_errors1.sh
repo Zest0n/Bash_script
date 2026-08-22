578a87616b3a0fa8153586bf43db3137c2cc95c7
Abhay Nawani <115456822+Zest0n@users.noreply.github.com>
2026-08-22 17:37:16 +0530

    Add files via upload

---
+++ 01-basics/07_errors1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "this is a test"
+
+set -x
+greet=hello
+echo "this is also a test"
+echo $greet
+set +x
---
+++ 01-basics/07_errors2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "this is a test"
+set -e
+greet=hello
+echo "this is also a test"
+echo $greet
+echo "trying to display a file that does not exist"
+cat sample1.txt
+echo "I am his real girlfriend"
